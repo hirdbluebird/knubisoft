1381e383556dd9a702d033af0e74cf83171125ef
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-03-29 23:01:48 +0300

    bootsrap added to the stylus compilation, images added

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,19 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var bootstrap = require('bootstrap-stylus');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-
-var autoprefixerOptions = {
-	browsers: ['> 1%']
-}
 
 gulp.task('stylus', function() {
 	return gulp.src('public/stylesheets/stylus/main.styl')
-			   .pipe(stylus())
-			   .pipe(autoprefixer(autoprefixerOptions))
-			   .pipe(cssmin())
-			   .pipe(rename({suffix: '.min'}))
-			   .pipe(gulp.dest('public/stylesheets/css'));
+				.pipe(stylus({
+						use: [
+							bootstrap()
+						]
+				}))
+				.pipe(cssmin())
+				.pipe(rename({suffix: '.min'}))
+				.pipe(gulp.dest('public/stylesheets/css'));
 });
 
 gulp.task('watch', function(){

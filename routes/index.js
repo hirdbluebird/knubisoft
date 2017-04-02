0d47abba7ac9d61289c5272a50e13c55a149403b
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-04-02 21:43:03 +0300

    content displaying from route render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	navLinks: ['home','catalog','shipping and payment','price','contacts'],
+  	slideCount: 3,
+  	breadcrumbs: ['Home','Catalog','Category Name'],
+  	sidebarMenu: [
+  		{
+  			menu: 'Cars',
+  			submenu: ['Mersedes', 'BMV', 'Aston Martin']
+  		},
+  		{
+  			menu: 'Household',
+  			submenu: ['Microwaves', 'Dishwashes','Fridges']
+  		},
+  		{
+  			menu: 'Computers',
+  			submenu: ['PC','Laptops']
+  		},
+  		{
+  			menu: 'Clothing',
+  			submenu: ['Shirts','Shoes','Socks','Jeans']
+  		}
+  	],
+  	itemPrice: ['550.95','350.95','850.95','550.95','350.95','850.95'],
+  	paginationCount: 7
+  	
+  });
 });
 
 module.exports = router;

d095f0908dcd5720321a32696aa3bebcc852fd1a
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-04-02 11:53:36 +0300

    mainpage full width pixel perfect markup finished

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,4 +5,5 @@ $(document).ready(function(){
 
 	$('.carousel-inner .item').first().addClass('active'); 
 	$('.carousel-indicators li').first().addClass('active');
+	$('.pagination li:nth-child(4)').addClass('active');
 })
